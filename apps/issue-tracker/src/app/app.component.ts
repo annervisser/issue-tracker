e79a522d51a04b966851310605145766e9b205ed
Anner Visser <annervisser@gmail.com>
2022-03-11 19:48:31 +0100

    setup nx

---
+++ apps/issue-tracker/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'issue-tracker-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'issue-tracker';
+}
