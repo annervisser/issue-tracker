04b5e29af6c8e376bbf091595bbafaf0e1e0deca
Anner Visser <annervisser@gmail.com>
2022-04-12 22:58:37 +0200

    Switch board component to use ngrx

--- libs/data-access/src/index.ts
+++ libs/data-access/src/index.ts
@@ -1,1 +1,5 @@
 export * from './lib/data-access.module';
+export * from './lib/services/story-client.service';
+export * from './lib/types/story';
+export * from './lib/services/state-client.service';
+export * from './lib/types/state';
--- libs/kanban-board/src/lib/board/board.component.ts
+++ libs/kanban-board/src/lib/board/board.component.ts
@@ -1,16 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-import { BoardService } from './board.service';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { BoardStore } from './board.store';
 
 @Component({
     selector: 'issue-tracker-board',
     templateUrl: './board.component.html',
     styleUrls: ['./board.component.scss'],
-    providers: [BoardService]
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    providers: [BoardStore]
 })
 export class BoardComponent implements OnInit {
-    constructor(public readonly boardService: BoardService) {
+    constructor(public readonly boardStore: BoardStore) {
+        this.boardStore.fetchStates();
     }
 
     ngOnInit(): void {
+    }
+
+    createStory(title: string, stateId: string) {
+        this.boardStore.createStory({ title, stateId });
     }
 }
